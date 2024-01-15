dcfcada7bb2064d99d471c8644010eba484aca47
SarahKovacs <147748094+SarahKovacs@users.noreply.github.com>
2024-01-15 15:59:03 +0100

    Update Second Barplot and Image

--- js/Vis.js
+++ js/Vis.js
@@ -1,7 +1,10 @@
+var colorScale = d3.scaleOrdinal()
+  .domain(["Gasoline", "Diesel", "Electric", "Electric/Diesel", "Electric/Gasoline"]) 
+  .range(["#232D3F", "#176B87", "#005B41", "#CD5C08", "#CD5C08"]); 
 
 // Definition der SVG-Größe und Margen
 var marginbm = {top: 20, right: 30, bottom: 40, left: 90},
-    widthbm = 700 - marginbm.left - marginbm.right,
+    widthbm = 1100 - marginbm.left - marginbm.right,
     heightbm = 500 - marginbm.top - marginbm.bottom;
 
 // SVG für Balkendiagramm erstellen
@@ -28,9 +31,17 @@ var svgmodel = d3.select("#ModelBarplot")
             let selectedOption = d3.select(this).select('option:checked');
             let selectedValue = selectedOption.attr('data-value'); // Auslesen des data-value Attributs
             console.log('Ausgewählte Marke:', selectedValue);
+
+            var modelEngineData = d3.nest()
+        .key(function(n) { return n.model; })
+        .key(function(n) { return n.fuel; })
+        .rollup(function(leaves) { return leaves.length; })
+        .entries(databm.filter(function(p) { return p.make === selectedValue; }));
     
             updateBarPlotBM(selectedValue, databm); // Aufruf der Funktion zum Aktualisieren des Balkendiagramms
         });
+
+        
     });
     
 
@@ -38,38 +49,61 @@ function updateBarPlotBM(selectedValue, databm) {
     // Löschen Sie vorherige Balken
     svgmodel.selectAll("*").remove();
 
-    var filteredData = d3.nest()
-        .key(function(p) { return p.model; })
-        .rollup(function(w) { return w.length; })
-        .entries(databm.filter(function(p) { return p.make === selectedValue; }));
+    var nestedData = d3.nest()
+    .key(function(d) { return d.model; })
+    .key(function(d) { return d.fuel; })
+    .rollup(function(leaves) { return leaves.length; })
+    .entries(databm.filter(function(d) { return d.make === selectedValue; }));
+
+var stackData = nestedData.map(function(d) {
+    var y0 = 0;
+    var fuels = d.values.map(function(dd) {
+        return { model: d.key, fuel: dd.key, count: dd.value, y0: y0, y1: y0 += dd.value };
+    });
+    return { model: d.key, fuels: fuels };
+});
+
     
-    // X-Achse
-    var x = d3.scaleBand()
-        .range([0, widthbm])
-        .domain(filteredData.map(function(p) { return p.key; }))
-        .padding(0.1);
-    svgmodel.append("g")
-        .attr("transform", "translate(0," + heightbm + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-          .attr("transform", "translate(-10,0)rotate(-45)")
-          .style("text-anchor", "end");
+// X-Achse
+var x = d3.scaleBand()
+    .range([0, widthbm])
+    .domain(stackData.map(function(p) { return p.model; }))
+    .padding(0.1);
+svgmodel.append("g")
+    .attr("transform", "translate(0," + heightbm + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
 
-    // Y-Achse
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(filteredData, function(p) { return p.value; })])
-        .range([heightbm, 0]);
-    svgmodel.append("g")
-        .call(d3.axisLeft(y));
+// Y-Achse
+var y = d3.scaleLinear()
+    .domain([0, d3.max(stackData, function(p) { 
+        return d3.sum(p.fuels, function(dd) { return dd.count; }); 
+    })])
+    .range([heightbm, 0]);
+svgmodel.append("g")
+    .call(d3.axisLeft(y));
 
-    // Balken
-    svgmodel.selectAll("rect")
-        .data(filteredData)
-        .enter()
-        .append("rect")
-        .attr("x", function(p) { return x(p.key); })
-        .attr("y", function(p) { return y(p.value); })
-        .attr("width", x.bandwidth())
-        .attr("height", function(p) { return heightbm - y(p.value); })
-        .attr("fill", "#4D8E9A");
+// Gruppen für jedes Modell
+var modelGroups = svgmodel.selectAll(".model-group")
+    .data(stackData)
+    .enter().append("g")
+    .attr("class", "model-group")
+    .attr("transform", function(d) { return "translate(" + x(d.model) + ",0)"; });
+
+// Erstellen der einzelnen Balken
+modelGroups.selectAll("rect")
+    .data(function(d) { return d.fuels; })
+    .enter().append("rect")
+    .attr("x", function(d, i, nodes) { 
+        return i * (x.bandwidth() / nodes.length);
+    })
+    .attr("width", x.bandwidth() / stackData[0].fuels.length)
+    .attr("y", function(d) { return y(d.count); })
+    .attr("height", function(d) { return heightbm - y(d.count); })
+    .attr("fill", function(d) { return colorScale(d.fuel); });
 }
+
+
+
